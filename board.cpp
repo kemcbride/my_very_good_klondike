5f1626b57c39963e7eac53beb0e7818c56499596
Kelly McBride <kemcbride28@gmail.com>
2020-04-23 19:35:38 -0400

    Now that everything else is moved out of board.*, implement board constructor.. annoying intiializer list..

--- board.cpp
+++ board.cpp
@@ -8,3 +8,16 @@
 
 using namespace std;
 
+Board::Board(Deck d) : tableau(d), stock(d) {
+  // Assuming you start with a full deck!
+  // First we fill the tableau.
+  /* Tableau t(d); */
+  /* // Then, using the same deck, we fill the stock. */
+  /* Stock s(d); */
+  // Then we create the foundations.
+  vector<Foundation> my_fdns;
+  for (int i =0; i < 4; ++i) {
+    my_fdns.push_back(Foundation());
+  }
+  this->foundations = my_fdns;
+}
--- board.h
+++ board.h
@@ -1,27 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include <algorithm> // random_shuffle
-#include <iostream>
-#include <string>
-#include <exception>
-#include <optional>
 #include <vector>
 
 #include "deck.h"
-#include "card.h"
 #include "foundation.h"
 #include "pile.h"
 #include "stock.h"
+#include "tableau.h"
 
-
-// The Tableau consists of 7 piles
-class Tableau {
-  public:
-    std::vector<Pile> piles;
-
-    Tableau(Deck);
-};
 
 class Board {
   private:
