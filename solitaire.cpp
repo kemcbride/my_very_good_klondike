dfaf215eb7d6484bdfa15e8fc611a1c9bed3339c
Kelly McBride <kemcbride28@gmail.com>
2020-04-23 17:11:43 -0400

    Clean up solitaire.cpp a little...

--- solitaire.cpp
+++ solitaire.cpp
@@ -17,16 +17,14 @@ int main(int argc, char** argv) {
   Deck deck;
 
   cout << "Shuffling and drawing top card a few times..." << endl;
-  for(int i = 0; i < 10; ++i) {
+  for(int i = 0; i < 5; ++i) {
     deck.shuffle();
     cout << deck.cards.at(0) << endl;
   }
 
   cout << "Now let's draw a few hands." << endl;
   for(int i=0; i < 3; ++i) {
-    vector<Card> hand = deck.draw(5);
-
-    for(auto c : hand) {
+    for(auto c : deck.draw(5)) {
       cout << c << " ";
     }
     cout << endl;
