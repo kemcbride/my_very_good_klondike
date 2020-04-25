c77174d9f197659bd19083d987a33978ab8e7053
Kelly McBride <kemcbride28@gmail.com>
2020-04-24 20:00:19 -0400

    Clean up solitaire.cpp

--- solitaire.cpp
+++ solitaire.cpp
@@ -14,26 +14,10 @@ using namespace std;
 
 int main(int argc, char** argv) {
   Deck deck;
-
-  cout << "Shuffling and drawing top card a few times..." << endl;
-  for(int i = 0; i < 5; ++i) {
-    deck.shuffle();
-    cout << deck.cards.at(0) << endl;
-  }
-
-  cout << "Now let's draw a few hands." << endl;
-  for(int i=0; i < 3; ++i) {
-    for(auto c : deck.draw(5)) {
-      cout << c << " ";
-    }
-    cout << endl;
-  }
-
-  deck.reset_index();
   deck.shuffle();
 
-  //Now let's just try declaring & instantiating some of these guys
+  // Now let's just try declaring & instantiating some of these guys
   Board b(deck);
-  cout << b.tableau.piles.front().runs.front().toString() << endl;
+  cout << b.tableau.toString() << endl;
 	return 0;
 }
