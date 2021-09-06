e87448b49b32f391d5953949dd8a1fe1a43a0eab
Kelly McBride <kemcbride28@gmail.com>
2021-09-06 15:23:23 -0700

    Autosolve actually working! Also an integration test case for it.

--- full_solve_tests.sh
+++ full_solve_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-# kill the process if it doesn't finish within 60 s - this is pretty generous
-timeout 1 cat inputs/seed2_cmds.txt | ./solitaire play 2
+# kill the process if it doesn't finish within 1 s - usually i see ~0.4s
+timeout 1 time cat inputs/seed2_cmds.txt | ./solitaire play 2 --autosolve=false
+timeout 1 time cat inputs/seed2_autosolve_cmds.txt | ./solitaire play 2 --autosolve=true
