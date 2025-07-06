fff2396acb690674e310cd1425e8fa3f717d08f8
Kelly McBride <kemcbride28@gmail.com>
2025-07-05 21:38:39 -0700

    [e2e] Add another e2e input & bump the timeout to 90s since its much slower

--- full_solve_tests.sh
+++ full_solve_tests.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 # kill the process if it doesn't finish within 1 s - usually i see ~0.4s
 # Above is no longer true since the implementation of the hint command.
-TIMEOUT_LIMIT=60
-# timeout 1 time cat inputs/seed2_cmds.txt | ./solitaire play 4 --autosolve=false
+TIMEOUT_LIMIT=90
 timeout $TIMEOUT_LIMIT sh -c "time ./solitaire play 4 < inputs/seed4_another_input.txt"
-# time ./solitaire play 4 < inputs/seed4_another_input.txt
 TIMEOUT_RESULT=$?
 if [[ $TIMEOUT_RESULT = 124 ]]; then
 	echo "Timed out running example full test with time limit ${TIMEOUT_LIMIT}s";
+	exit $TIMEOUT_RESULT
 else
 	echo "Did not time out yay";
 fi
-exit $TIMEOUT_RESULT
+
+# Repeat for next test input - TODO should function-ize this
+timeout $TIMEOUT_LIMIT sh -c "time ./solitaire play 7 < inputs/seed6_input.txt"
+TIMEOUT_RESULT=$?
+if [[ $TIMEOUT_RESULT = 124 ]]; then
+	echo "Timed out running example full test with time limit ${TIMEOUT_LIMIT}s";
+	exit $TIMEOUT_RESULT
+else
+	echo "Did not time out yay";
+fi
