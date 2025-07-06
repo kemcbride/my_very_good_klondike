6ab1b059973f3543147659aee1856ee055ce5fc5
Kelly McBride <kemcbride28@gmail.com>
2025-07-05 21:12:00 -0700

    [e2e] Update timeout and message to 60s for CI?

--- full_solve_tests.sh
+++ full_solve_tests.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # kill the process if it doesn't finish within 1 s - usually i see ~0.4s
 # Above is no longer true since the implementation of the hint command.
-TIMEOUT_LIMIT=30
+TIMEOUT_LIMIT=60
 # timeout 1 time cat inputs/seed2_cmds.txt | ./solitaire play 4 --autosolve=false
 timeout $TIMEOUT_LIMIT sh -c "time ./solitaire play 4 < inputs/seed4_another_input.txt"
 # time ./solitaire play 4 < inputs/seed4_another_input.txt
 TIMEOUT_RESULT=$?
 if [[ $TIMEOUT_RESULT = 124 ]]; then
-	echo "Timed out running example full test with time limit 10s";
+	echo "Timed out running example full test with time limit ${TIMEOUT_LIMIT}s";
 else
 	echo "Did not time out yay";
 fi
